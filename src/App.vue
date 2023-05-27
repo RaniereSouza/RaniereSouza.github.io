1468ed86781c3447768d22e870534b1777153573
Raniere de Souza Santos <raniere.souza.cc@gmail.com>
2023-05-27 02:34:56 -0300

    Trying to solve the routing problem with the 404.html file on GitHub Pages

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,19 @@
 <template>
   <router-view></router-view>
 </template>
+
+<script setup>
+  import { onBeforeMount } from 'vue';
+  import { useRouter } from 'vue-router';
+
+  onBeforeMount(() => {
+    const router = useRouter();
+    const path = localStorage.getItem('@ranieresouza.github.io:path')?.trim();
+
+    if (path) {
+      router
+        .replace(path)
+        .finally(() => localStorage.removeItem('@ranieresouza.github.io:path'));
+    }
+  });
+</script>
