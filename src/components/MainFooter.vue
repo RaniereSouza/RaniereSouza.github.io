a32120bc57c48ba3e6fd244115b17fd5069071ec
Raniere de Souza Santos <raniere.souza.cc@gmail.com>
2024-12-20 02:21:07 -0300

    Main page footer is now a separate component

---
+++ src/components/MainFooter.vue
@@ -0,0 +1,79 @@
+<template>
+  <footer>
+    &copy;Raniere Souza Santos, {{currentYear}}
+    <a target="_blank" href="mailto:raniere.souza.cc@gmail.com" v-tooltip="'Email'">
+      <span class="material-icons">alternate_email</span>
+    </a>
+    <a target="_blank" href="https://t.me/raniere_souza" v-tooltip="'Telegram'">
+      <span class="material-icons">send</span>
+    </a>
+    <a target="_blank" href="https://wa.me/5571991655209" v-tooltip="'WhatsApp'">
+      <span class="material-icons">phone</span>
+    </a>
+    <a target="_blank" href="https://github.com/RaniereSouza" v-tooltip="'GitHub'">
+      <img src="../assets/github-mark-white.svg" alt="GH">
+    </a>
+  </footer>
+</template>
+
+<script setup>
+  import vTooltip from './directives/vTooltip';
+
+  const currentYear = (new Date()).getFullYear();
+</script>
+
+<style scoped>
+  footer {
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+    height: var(--footer-height);
+    padding: var(--default-spacing);
+    text-align: center;
+    width: min(100vw, 100%);
+    font-size: .75rem;
+  }
+
+  footer a {
+    padding: 0 1.5rem;
+    border-left: 1px dashed #fff;
+    color: var(--font-color-light);
+    text-decoration: none;
+    display: inline-flex;
+    align-items: center;
+  }
+
+  footer a:first-of-type {
+    margin-left: 1.5rem;
+  }
+
+  footer a .material-icons {
+    font-size: 1.25rem;
+  }
+
+  footer a img {
+    height: 1.25rem;
+  }
+
+  @media screen and (max-width: 575px) {
+    footer {
+      padding: var(--default-spacing-y);
+    }
+
+    footer a {
+      padding: 0 .75rem;
+    }
+
+    footer a:first-of-type {
+      margin-left: .75rem;
+    }
+
+    footer a .material-icons {
+      font-size: 1.125rem;
+    }
+
+    footer a img {
+      height: 1.125rem;
+    }
+  }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -33,21 +33,7 @@
       <contact />
     </section>
 
-    <footer>
-      &copy;Raniere Souza Santos, {{currentYear}}
-      <a target="_blank" href="mailto:raniere.souza.cc@gmail.com" v-tooltip="'Email'">
-        <span class="material-icons">alternate_email</span>
-      </a>
-      <a target="_blank" href="https://t.me/raniere_souza" v-tooltip="'Telegram'">
-        <span class="material-icons">send</span>
-      </a>
-      <a target="_blank" href="https://wa.me/5571991655209" v-tooltip="'WhatsApp'">
-        <span class="material-icons">phone</span>
-      </a>
-      <a target="_blank" href="https://github.com/RaniereSouza" v-tooltip="'GitHub'">
-        <img src="../assets/github-mark-white.svg" alt="GH">
-      </a>
-    </footer>
+    <main-footer />
   </main>
 </template>
 
@@ -56,16 +42,13 @@
 
   import { remToPx } from '../lib/helpers';
 
-  import vTooltip from '../components/directives/vTooltip';
-
   import NavHeader from '../components/NavHeader.vue';
   import Waves from '../components/animations/Waves.vue';
   import WhoAmI from '../components/WhoAmI.vue';
   import Carousel from '../components/Carousel.vue';
   import Resume from '../components/Resume.vue';
   import Contact from '../components/Contact.vue';
-
-  const currentYear = (new Date()).getFullYear();
+  import MainFooter from '../components/MainFooter.vue';
 
   const showNav = ref();
   function toggleShowNav() {
@@ -253,59 +236,4 @@
     background-size: max(50vw, 50vh) auto;
     background-position: center center;
   }
-
-  footer {
-    display: inline-flex;
-    align-items: center;
-    justify-content: center;
-    height: var(--footer-height);
-    padding: var(--default-spacing);
-    text-align: center;
-    width: min(100vw, 100%);
-    font-size: .75rem;
-  }
-
-  footer a {
-    padding: 0 1.5rem;
-    border-left: 1px dashed #fff;
-    color: var(--font-color-light);
-    text-decoration: none;
-    display: inline-flex;
-    align-items: center;
-  }
-
-  footer a:first-of-type {
-    margin-left: 1.5rem;
-  }
-
-  footer a .material-icons {
-    font-size: 1.25rem;
-  }
-
-  footer a img {
-    height: 1.25rem;
-  }
-
-  @media screen and (max-width: 575px) {
-    footer {
-      padding: var(--default-spacing-y);
-    }
-
-    footer a {
-      padding: 0 .75rem;
-    }
-
-    footer a:first-of-type {
-      margin-left: .75rem;
-    }
-
-    footer a .material-icons {
-      font-size: 1.125rem;
-    }
-
-    footer a img {
-      height: 1.125rem;
-    }
-  }
 </style>
-
