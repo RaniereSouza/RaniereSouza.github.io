58e02f997def72012c4a9999a3e498736892e62e
Raniere de Souza Santos <raniere.souza.cc@gmail.com>
2025-04-25 02:00:56 -0300

    Stablishing environment variable to toggle Maintenance Mode for the page

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
-const routes = [
+const maintenanceModeOn =
+  import.meta.env.VITE_MAINTENANCE_MODE === 'true' ||
+  import.meta.env.VITE_MAINTENANCE_MODE === 'on' ||
+  import.meta.env.VITE_MAINTENANCE_MODE === 'ok'
+
+const routes = !maintenanceModeOn ? [
   {path: '/',               name: 'initial',     alias: ['/index.html'], redirect: '/home'},
   {path: '/home',           name: 'home',        component: () => import('../views/Home.vue')},
   {path: '/maintenance',    name: 'maintenance', component: () => import('../views/Maintenance.vue'), alias: ['/work-in-progress']},
   {path: '/:pathMatch(.*)', name: 'not-found',   component: () => import('../views/NotFound.vue')},
+] : [
+  {path: '/:pathMatch(.*)', name: 'maintenance', component: () => import('../views/Maintenance.vue'), alias: ['/work-in-progress']},
 ];
 
 export default createRouter({
